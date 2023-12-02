79c464628bb5fb8d9b0167522d8910d7f3ac7efa
Ylniss <zupqa0@gmail.com>
2023-12-02 11:18:38 +0100

    add git branch commands, mkdircd; change parenthesis style

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,29 +6,33 @@ $downloadsPath = "~/Downloads/"
 #           NAVIGATION
 # --------------------------------
 
-function FindDirOrFileName()
-{
+function FindDirOrFileName() {
     param([string]$path, [string]$dirOrFileName)
 	Get-Childitem -Path "$path" -Include *$dirOrFileName* -Recurse -ErrorAction SilentlyContinue
 }
 
-function ChangeDirToRepo()
-{
+function ChangeDirToRepo() {
     Set-Location $repoPath
 }
 
-function ChangeDirToGames()
-{
+function ChangeDirToGames() {
     Set-Location $gamesPath
 }
 
-function ChangeDirToDownloads()
-{
+function ChangeDirToDownloads() {
     Set-Location $downloadsPath
 }
 
-function OpenExplorer()
-{
+function NewDirectoryAndEnter {
+    param([string]$dirName)
+
+    $newPath = Join-Path -Path (Get-Location) -ChildPath $dirName
+    New-Item -Path $newPath -ItemType Directory -Force
+
+    Set-Location -Path $newPath
+}
+
+function OpenExplorer() {
 	param([string] $path)
 	if($path) {
 		explorer $path
@@ -37,27 +41,29 @@ function OpenExplorer()
 	}
 }
 
+
+
 Set-Alias -Name fnd   -Value FindDirOrFileName
-Set-Alias -Name repo  -Value ChangeDirToRepo
-Set-Alias -Name games -Value ChangeDirToGames
-Set-Alias -Name dwn   -Value ChangeDirToDownloads
-Set-Alias -Name e     -Value OpenExplorer
+
+Set-Alias -Name repo  	-Value ChangeDirToRepo
+Set-Alias -Name games 	-Value ChangeDirToGames
+Set-Alias -Name dwn   	-Value ChangeDirToDownloads
+Set-Alias -Name mkdircd -Value NewDirectoryAndEnter
+
+Set-Alias -Name e	-Value OpenExplorer
 
 
 # --------------------------------
 #           DEVELOPMENT
 # --------------------------------
 
-function OpenWithNotepad()
-{
+function OpenWithNotepad() {
     param([string] $path)
     start notepad++ $path
 }
 
 function StartDevTools {
-    param (
-        [switch]$f
-    )
+    param ([switch]$f) # start WebStorm for frontend development
 
     $riderPath = Get-ChildItem -Path "$env:ProgramFiles\JetBrains\*Rider *\bin\rider*.exe" -File | Select-Object -First 1 -ExpandProperty FullName
     $dataGripPath = Get-ChildItem -Path "$env:ProgramFiles (x86)\JetBrains\*DataGrip *\bin\datagrip*.exe" -File | Select-Object -First 1 -ExpandProperty FullName
@@ -90,13 +96,11 @@ function StartDevTools {
 
 # ----- GIT -----
 
-function GitStatus 
-{ 
+function GitStatus { 
 	git status 
 }
 
-function GitAddAll 
-{ 
+function GitAddAll { 
 	param([string] $path)
 	
 	if($path) {
@@ -107,47 +111,72 @@ function GitAddAll
 }
 
 function GitDiff {
-	
 	$gitDiffOutput = git diff
 
-    if (![string]::IsNullOrWhiteSpace($gitDiffOutput))
-	{
+    if (![string]::IsNullOrWhiteSpace($gitDiffOutput)) {
 		Write-Host "Unstaged Changes:" -ForegroundColor Red
 		git diff
     } 
 	
 	$gitDiffStagedOutput = git diff --staged
 	
-	if (![string]::IsNullOrWhiteSpace($gitDiffStagedOutput))
-	{
+	if (![string]::IsNullOrWhiteSpace($gitDiffStagedOutput)) {
 		Write-Host "Staged Changes:" -ForegroundColor Green
 		git diff --staged
     }  
 }
 
-function GitCommitMessage 
-{ 
+function GitCommitMessage { 
 	param($message); 
 	git commit -m $message 
 }
 
-function GitPushOrigin 
-{ 
+function GitPushOrigin { 
 	git push -u origin
+}
+
+function GitBranch { 
+    param(
+        [Parameter(Mandatory=$false)] [string]$newBranchName,
+        [Parameter(Mandatory=$false)] [switch]$a, # list all branches
+        [Parameter(Mandatory=$false)] [switch]$d, # delete branch
+		[Parameter(Mandatory=$false)] [string]$branchNameToDelete
+    )
+
+    if ($a) {
+        git branch -a
+    }
+    elseif ($d -and $branchNameToDelete) {
+        git branch -d $branchNameToDelete
+    }
+	elseif ($newBranchName) {
+		git branch $newBranchName
+	}
+    else {
+        Write-Host "Please specify an operation or a new branch name."
+    }
+}
+
+function GitCheckout {
+    param($branchName); 
+    git checkout $branchName
+}
+
+function GitBranchCheckout {
+    param($newBranchName); 
+	git branch $newBranchName
+    git checkout $newBranchName
 }
 
 # ----- DOCKER -----
 
-function DockerComposeUp 
-{ 
+function DockerComposeUp { 
 	docker compose up
 }
 
-function DockerComposeUpBuild
-{ 
+function DockerComposeUpBuild { 
 	docker compose up --build
 }
-
 
 Set-Alias -Name ntpd -Value OpenWithNotepad
 Set-Alias -Name dev  -Value StartDevTools
@@ -157,16 +186,19 @@ Set-Alias -Name gita -Value GitAddAll
 Set-Alias -Name gitd -Value GitDiff
 Set-Alias -Name gitc -Value GitCommitMessage
 Set-Alias -Name gitp -Value GitPushOrigin
+Set-Alias -Name gitb -Value GitBranch
+Set-Alias -Name gitch -Value GitCheckout
+Set-Alias -Name gitbch -Value GitBranchCheckout
 
-Set-Alias -Name dckrcu -Value DockerComposeUp
+Set-Alias -Name dckrcu  -Value DockerComposeUp
 Set-Alias -Name dckrcub -Value DockerComposeUpBuild
+
 
 # --------------------------------
 #             GAMES
 # --------------------------------
 
-function StartRa2WithAutoClicker()
-{
+function StartRa2WithAutoClicker() {
 	$originalPath = Get-Location
 	start "$env:ProgramFiles (x86)\Red Alert 2 Blitz Autoclicker\autoclicker.exe"
 	Set-Location "$gamesPath\Command and Conquer Red Alert II"
@@ -188,7 +220,6 @@ function symlink () {
 oh-my-posh init pwsh | Invoke-Expression
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile))
-{
+if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
