38ee59c7034447b4a524a5ec00d26e6ee2f586c9
ylniss <zupqa0@gmail.com>
2023-12-27 22:51:04 +0100

    wezterm tab numbers, refactor; mason config in separate file, cursor line

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,6 +13,7 @@ if is_windows then
   config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
 end
 
+config.window_decorations = "RESIZE"
 config.window_close_confirmation = 'NeverPrompt'
 
 config.show_new_tab_button_in_tab_bar = false
@@ -83,7 +84,7 @@ wezterm.on(
       { Text = wezterm.nerdfonts.pl_right_hard_divider },
       { Background = { Color = background } },
       { Foreground = { Color = foreground } },
-      { Text = " " .. title .. " " },
+      { Text = " " .. tostring(tab.tab_index + 1) .. ": " .. title .. " " },
       { Background = { Color = edge_background } },
       { Foreground = { Color = edge_foreground } },
       { Text = wezterm.nerdfonts.pl_left_hard_divider },
@@ -96,30 +97,30 @@ wezterm.on(
 
 config.keys = {
   -- Ctrl + number to focus on <number> tab
-  { key = "1", mods = "CTRL", action = wezterm.action { ActivateTab = 0 }},
-  { key = "2", mods = "CTRL", action = wezterm.action { ActivateTab = 1 }},
-  { key = "3", mods = "CTRL", action = wezterm.action { ActivateTab = 2 }},
-  { key = "4", mods = "CTRL", action = wezterm.action { ActivateTab = 3 }},
-  { key = "5", mods = "CTRL", action = wezterm.action { ActivateTab = 4 }},
-  { key = "6", mods = "CTRL", action = wezterm.action { ActivateTab = 5 }},
-  { key = "7", mods = "CTRL", action = wezterm.action { ActivateTab = 6 }},
-  { key = "8", mods = "CTRL", action = wezterm.action { ActivateTab = 7 }},
-  { key = "9", mods = "CTRL", action = wezterm.action { ActivateTab = -1 }},
+  { key = "1", mods = "CTRL", action = wezterm.action { ActivateTab = 0 } },
+  { key = "2", mods = "CTRL", action = wezterm.action { ActivateTab = 1 } },
+  { key = "3", mods = "CTRL", action = wezterm.action { ActivateTab = 2 } },
+  { key = "4", mods = "CTRL", action = wezterm.action { ActivateTab = 3 } },
+  { key = "5", mods = "CTRL", action = wezterm.action { ActivateTab = 4 } },
+  { key = "6", mods = "CTRL", action = wezterm.action { ActivateTab = 5 } },
+  { key = "7", mods = "CTRL", action = wezterm.action { ActivateTab = 6 } },
+  { key = "8", mods = "CTRL", action = wezterm.action { ActivateTab = 7 } },
+  { key = "9", mods = "CTRL", action = wezterm.action { ActivateTab = -1 } },
 
   -- Ctrl + T to open a new tab
-  { key = "t", mods = "CTRL", action = wezterm.action { SpawnTab = "CurrentPaneDomain" }},
+  { key = "t", mods = "CTRL",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
 
   -- Ctrl + Shift + v to split vertically
-  { key = "v", mods = "CTRL|SHIFT", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" }}},
+  { key = "v", mods = "CTRL|SHIFT", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
 
   -- Ctrl + Shift + h to split vertically
-  { key = "h", mods = "CTRL|SHIFT", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" }}},
+  { key = "h", mods = "CTRL|SHIFT", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
 
   -- Ctrl + Alt + Arrow to resize in arrow direction
-  { key = "UpArrow", mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Up", 1 }}},
-  { key = "DownArrow", mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Down", 1 }}},
-  { key = "LeftArrow", mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Left", 1 }}},
-  { key = "RightArrow", mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Right", 1 }}},
+  { key = "UpArrow",    mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Up", 1 } } },
+  { key = "DownArrow",  mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Down", 1 } } },
+  { key = "LeftArrow",  mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Left", 1 } } },
+  { key = "RightArrow", mods = "CTRL|ALT", action = wezterm.action { AdjustPaneSize = { "Right", 1 } } },
 
   { key = 'F11', action = wezterm.action.ToggleFullScreen },
 
