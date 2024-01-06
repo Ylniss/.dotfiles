85acdc9a8431ca31b8ceb241c3c42a2b99928ef7
ylniss <zupqa0@gmail.com>
2024-01-06 21:40:27 +0100

    enchance install dotfiles script

--- scripts/windows/install_dotfiles_win.ps1
+++ scripts/windows/install_dotfiles_win.ps1
@@ -1,16 +1,23 @@
 $dotfilesRepoDir = "$HOME/stuff/repo/.dotfiles"
 
-Write-Host "Creating symbolic link for nvim"
-New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$dotfilesRepoDir\nvim"
+function CreateSymbolicLink($linkPath, $targetPath, $description)
+{
+    if (Test-Path $linkPath)
+    {
+        Write-Host "$description symbolic link already exists."
+    } else
+    {
+        Write-Host "Creating symbolic link for $description"
+        New-Item -ItemType SymbolicLink -Path $linkPath -Target $targetPath
+    }
+}
 
-Write-Host "Creating symbolic link for .ideavimrc"
-New-Item -ItemType SymbolicLink -Path "$HOME\.ideavimrc" -Target "$dotfilesRepoDir\.ideavimrc"
-
-Write-Host "Creating symbolic link for .wezterm.lua"
-New-Item -ItemType SymbolicLink -Path "$HOME\.wezterm.lua" -Target "$dotfilesRepoDir\.wezterm.lua"
-
-Write-Host "Creating symbolic link for .zshrc"
-New-Item -ItemType SymbolicLink -Path "$HOME\.zshrc" -Target "$dotfilesRepoDir\.zshrc"
+CreateSymbolicLink "$env:LOCALAPPDATA\nvim" "$dotfilesRepoDir\nvim" "nvim"
+CreateSymbolicLink "$HOME\.ideavimrc" "$dotfilesRepoDir\.ideavimrc" ".ideavimrc"
+CreateSymbolicLink "$HOME\.wezterm.lua" "$dotfilesRepoDir\.wezterm.lua" ".wezterm.lua"
+CreateSymbolicLink "$HOME\.zshrc" "$dotfilesRepoDir\.zshrc" ".zshrc"
+CreateSymbolicLink "$env:LOCALAPPDATA\lf\lfrc" "$dotfilesRepoDir\lf\windows\lfrc" "lf (lfrc)"
+CreateSymbolicLink "$env:LOCALAPPDATA\lf\icons" "$dotfilesRepoDir\lf\windows\icons" "lf (icons)"
 
 Write-Host "Creating symbolic link for Microsoft.PowerShell_profile.ps1"
 if (Test-Path $PROFILE) {
@@ -21,6 +28,3 @@ New-Item -ItemType SymbolicLink -Path $PROFILE -Target "$dotfilesRepoDir\Microso
 Write-Host "Setting environment variable for .ripgreprc"
 setx RIPGREP_CONFIG_PATH "$dotfilesRepoDir\.ripgreprc"
 
-Write-Host "Creating symbolic link for lf"
-New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\lf\lfrc" -Target "$dotfilesRepoDir\lf\windows\lfrc"
-New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\lf\icons" -Target "$dotfilesRepoDir\lf\windows\icons"
