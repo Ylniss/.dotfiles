69fd27de79111888d3161e8f5e2c5b2ca6e0475d
ylniss <zupqa0@gmail.com>
2024-02-10 17:47:20 +0100

    formatting; update packages

--- nvim/after/plugin/lsp/lspsaga.lua
+++ nvim/after/plugin/lsp/lspsaga.lua
@@ -1,12 +1,12 @@
-require('lspsaga').setup({
-  symbol_in_winbar = {
-    enable = false
-  },
-  finder = {
-    max_height = 0.6,
-    keys = {
-      vsplit = 'v',
-      toggle_or_open = 'l'
-    }
-  }
+require("lspsaga").setup({
+	symbol_in_winbar = {
+		enable = false,
+	},
+	finder = {
+		max_height = 0.6,
+		keys = {
+			vsplit = "v",
+			toggle_or_open = "l",
+		},
+	},
 })
