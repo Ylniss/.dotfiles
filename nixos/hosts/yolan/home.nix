fdc1ec16980c2c5b0d21969a03f188f008d4c986
ylniss <zupqa0@gmail.com>
2024-12-21 14:41:16 +0100

    symlink nvim config in nixos

--- nixos/hosts/yolan/home.nix
+++ nixos/hosts/yolan/home.nix
@@ -22,6 +22,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/nvim".source = ../../../nvim;
+    ".config/nvim".recursive = true;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
