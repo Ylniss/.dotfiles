407bc026da7758a49f7f0582406a4c34cae78579
Ylniss <zupqa0@gmail.com>
2024-12-28 21:10:54 +0100

    add xclip to psw.nix

--- nixos/packages/cli/psw.nix
+++ nixos/packages/cli/psw.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   home.packages = with pkgs; [
+    xclip # needed dependency for clipboard management
     inputs.psw.packages.${system}.psw
     inputs.psw.packages.${system}.clipclean
   ];
