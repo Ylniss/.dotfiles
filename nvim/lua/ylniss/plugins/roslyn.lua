509f7ec1a92f5f315a3cb97338f4b2cfc09f3671
ylniss <zupqa0@gmail.com>
2026-05-18 06:06:05 +0200

    update roslyn plugin, sync packages

--- nvim/lua/ylniss/plugins/roslyn.lua
+++ nvim/lua/ylniss/plugins/roslyn.lua
@@ -3,9 +3,25 @@
 -- C# language server (Microsoft's official Roslyn LSP)
 -- ========================================================
 return {
-	"seblj/roslyn.nvim",
+	"seblyng/roslyn.nvim",
 	ft = "cs",
 	opts = {
 		filewatching = "off",
+		args = {
+			"--stdio",
+			"--logLevel=Information",
+			"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.log.get_filename()),
+		},
 	},
+	config = function(_, opts)
+		require("roslyn").setup(opts)
+
+		-- Register the format-on-save autocommand
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.cs",
+			callback = function(args)
+				vim.lsp.buf.format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
